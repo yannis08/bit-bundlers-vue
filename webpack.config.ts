9245a5830c240822691bbf542fc24279b8ec2203
yannis <yannis@wiin-project.com>
2020-07-09 00:57:54 +0200

    init compiler

---
+++ webpack.config.ts
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+import nodeExternals from 'webpack-node-externals';
+import getFilesList from './getFilesList';
+
+// Import so it will be added as a dependency on Bit.
+import presetEnv from '@babel/preset-env';
+import Vinyl from 'vinyl';
+import VueLoaderPlugin from 'vue-loader/lib/plugin';
+import presetVueJsx from '@vue/babel-preset-jsx';
+
+type LibraryTarget = 'umd';
+
+const modulesPath = path.normalize(`${__dirname}${path.sep}..${path.sep}node_modules`);
+
+const getConfig = (files: Vinyl[], distPath: string) => {
+  return {
+    entry: getFilesList(files),
+    context: `${__dirname}${path.sep}..`,
+    resolve: {
+      modules: [modulesPath, 'node_modules'],
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    resolveLoader: {
+      modules: [modulesPath, 'node_modules']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            transpileOptions: {
+              transforms: {
+                modules: false
+              }
+            }
+          }
+        },
+        // this will apply to both plain `.js` files
+        // AND `<script>` blocks in `.vue` files
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [presetEnv, presetVueJsx]
+          }
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/]
+          }
+        },
+        // this will apply to both plain `.css` files
+        // AND `<style>` blocks in `.vue` files
+        {
+          test: /\.css$/,
+          use: ['vue-style-loader', 'css-loader']
+        },
+        {
+          test: /\.s[a|c]ss$/,
+          loader: ['vue-style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.less$/,
+          loader: ['vue-style-loader', 'css-loader', 'less-loader']
+        }
+      ]
+    },
+    plugins: [
+      // make sure to include the plugin for the magic
+      new VueLoaderPlugin()
+    ],
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(distPath),
+      filename: '[name].js',
+      libraryTarget: 'umd' as LibraryTarget
+    }
+  };
+};
+export default getConfig;
